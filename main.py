c42d65569aa7cb38a27ca60266969e3efa75c085
Dalton <daltonfiggdavies@gmail.com>
2024-01-13 15:26:47 +0000

    Develop main story

--- main.py
+++ main.py
@@ -40,7 +40,11 @@ def main_menu():
 
 
 def new_game():
-    print('New game')
+    print('War never changes.')
+    print('Awakening in a cluttered shelter with blurred vision and fragmented recollections, you gradually regain clarity.')
+    print('As your sight refines, a chilling revelation unfolds - a dusty skeleton is sprawled nearby')
+    print('You feel a jolt of horror... how did you get here?')
+    print('As you glance around, a weathered journal lies nestled amidst the debris. Its timeworn pages beckon with the promise of untold secrets and potential answers.')
 
 
 def load_game():
